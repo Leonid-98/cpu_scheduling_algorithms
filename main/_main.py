663027b35aa0c3e5b64abce88083ffb4701820c2
LeonidTsigrinski <Leonid.tsigrinski@gmail.com>
2021-10-09 16:29:50 +0300

    minor edit

--- main/_main.py
+++ main/_main.py
@@ -33,8 +33,13 @@ class MyGui(Frame):
         srtf_btn = Button(self.outercanvas, text="SRTF", font=self.font, command=lambda: self.calculate_schedue_and_draw("srtf"))
         self.outercanvas.create_window(320, 140, anchor=NW, height=30, width=80, window=srtf_btn)
 
-        clear_btn = Button(self.outercanvas, text="Clear", font=self.font, command=lambda: self.innercanvas.delete("all"))
+        clear_btn = Button(self.outercanvas, text="Clear", font=self.font, command=lambda: self.clear_inner_canvas())
         self.outercanvas.create_window(420, 140, anchor=NW, height=30, width=80, window=clear_btn)
+
+        self.awt_label = Label(self.outercanvas, text="Keskmine ootamis aeg: --", font=self.font, bg="#e1e8f4")
+        self.outercanvas.create_window(250, 180, anchor=NW, height=30, width=200, window=self.awt_label)
+        
+
 
         self.entry = Entry(self.outercanvas, font=self.font)
         self.entry.insert(END, "1,10;3,3;4,1;8,6;15,2")
@@ -55,6 +60,11 @@ class MyGui(Frame):
             "P12": "#D43366",
         }
 
+    def clear_inner_canvas(self):
+        self.innercanvas.delete("all")
+        self.awt_label["text"]= "Keskmine ootamis aeg: --"
+
+
     def convert_string_to_process_queue(self, string):
         # abifunktsioon mis teisendab sisend kujuks: str "1,0;2,3" --> list [[1, 0], [2, 3]] ning kohe sorrteerib saabumise aja kaudu
         return sorted([[int(time) for time in process.split(",")] for process in string.split(";")], key=itemgetter(0))
@@ -68,7 +78,7 @@ class MyGui(Frame):
         self.innercanvas.create_text(x2 - 15, 90, text=completion_time, font=self.font)
 
     def calculate_schedue_and_draw(self, type: str):
-        self.innercanvas.delete("all")
+        self.clear_inner_canvas()
         processes_queue = self.convert_string_to_process_queue(self.entry.get())
 
         target = None
@@ -85,6 +95,7 @@ class MyGui(Frame):
         awt = target.get_awt()
         last_tact = execution_order[-1][-1][-1]
 
+        self.awt_label["text"]= "Keskmine ootamis aeg: " + str(awt)
         # div = palju pikslit ühes taktis
         div = int(round(1500 / last_tact))
         for process in execution_order:
